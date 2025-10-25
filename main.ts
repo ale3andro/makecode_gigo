db13858c4c7ddda668d8b9b7588a9adf7fe5df07
ale3andro <ale3andro@gmail.com>
2025-10-25 11:58:52 +0300

    changes

--- main.ts
+++ main.ts
@@ -22,18 +22,25 @@ namespace alxGigo {
         P12 = DigitalPin.P12
     }
 
+    export enum LedState {
+        //% block="άναψε"
+        άναψε = 1,
+        //% block="σβήσε"
+        σβήσε = 0
+    }
+
     /**
      * Turn LED ON
      * @param pin the pin where LED is connected
      */
-    //% block="turn ON LED at pin %pin"
+    //% block="LED στη θύρα %pin %led_state"
     //% group="LED"
     //% weight=100
     //% pin.fieldEditor="gridpicker"
     //% pin.fieldOptions.columns=3
     //% pin.fieldOptions.width=220
-    export function turnOn(pin: LedPin): void {
-        pins.digitalWritePin(pin, 1)
+    export function turnOn(pin: LedPin, led_state: LedState): void {
+        pins.digitalWritePin(pin, led_state)
     }
 
     /**
